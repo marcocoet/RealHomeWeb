a9c46eac9a4480f4db4b3a7663ccccd89d994510
Timothy de Villiers <Timothy.deVilliers@avenutech.com>
2024-10-22 19:56:24 +0200

    Fix api

--- src/api/api.js
+++ src/api/api.js
@@ -1,18 +1,17 @@
 import conf from "../config/config.json";
 import Utils from "./utils";
-import { EventEmitter } from 'fbemitter';
-import ErrorMessageFormatter from '../error-message.js/error-messages';
+import { EventEmitter } from "fbemitter";
+import ErrorMessageFormatter from "../error-message.js/error-messages";
 
 const emitter = new EventEmitter();
-
 
 class Api {
   constructor() {
     this.showServerError = Utils.debounce((message) => {
-      emitter.emit('toast', {
-        type: 'Error',
-        from: 'Api',
-        message: message || 'Error: ',
+      emitter.emit("toast", {
+        type: "Error",
+        from: "Api",
+        message: message || "Error: ",
         duration: 10000,
       });
     }, 400);
@@ -20,44 +19,40 @@ class Api {
 
   setToken(token) {
     this.token = token;
-    localStorage.setItem('token', token);
+    localStorage.setItem("token", token);
   }
 
   getToken() {
-    return localStorage.getItem('token');
+    return localStorage.getItem("token");
   }
 
   callApi(method, options) {
-    let completeUrl = options && options.completeUrl
-      ? options.completeUrl
-      : conf['realhome'] + options.uri;
-
-    console.log('conf.realhome:', conf['realhome']);
-    console.log('options.uri:', options.uri);
-    console.log('completeUrl:', completeUrl);
-
+    let completeUrl =
+      options && options.completeUrl
+        ? options.completeUrl
+        : conf["realhome"] + options.uri;
     let headers = {
-      'Content-Type': options && options.contentType
-        ? options.contentType
-        : 'application/json',
-      Accept: options && options.acceptType
-        ? options.acceptType
-        : 'application/json',
+      "Content-Type":
+        options && options.contentType
+          ? options.contentType
+          : "application/json",
+      Accept:
+        options && options.acceptType ? options.acceptType : "application/json",
     };
 
     const token = this.getToken();
     if (token) {
-      headers['Authorization'] = 'Bearer ' + token;
+      headers["Authorization"] = "Bearer " + token;
     }
 
-    completeUrl = completeUrl.replace(/[&]{0,1}[a-zA-Z0-9]*=undefined/g, '');
+    completeUrl = completeUrl.replace(/[&]{0,1}[a-zA-Z0-9]*=undefined/g, "");
 
     if (options.contentType === false) {
-      delete headers['Content-Type'];
+      delete headers["Content-Type"];
     }
 
     if (options.acceptType === false) {
-      delete headers['Accept'];
+      delete headers["Accept"];
     }
 
     let config = {
@@ -73,15 +68,18 @@ class Api {
     }
 
     if (options.data) {
-      if (method !== 'get') {
-        if (options.raw || (options.contentType && options.contentType !== 'application/json')) {
+      if (method !== "get") {
+        if (
+          options.raw ||
+          (options.contentType && options.contentType !== "application/json")
+        ) {
           config.body = options.data;
         } else {
           config.body = JSON.stringify(options.data);
         }
       } else {
         let urlParams = new URLSearchParams(Object.entries(options.data));
-        completeUrl += '?' + urlParams;
+        completeUrl += "?" + urlParams;
       }
     }
 
@@ -91,40 +89,57 @@ class Api {
           if (response.ok) {
             if (response.status === 204) {
               resolve();
-            } else if (response.headers.get('Content-Type').indexOf('application/json') !== -1) {
+            } else if (
+              response.headers
+                .get("Content-Type")
+                .indexOf("application/json") !== -1
+            ) {
               const responseClone = response.clone();
               resolve(responseClone.json());
             } else {
-              response.blob().then((blob) => {
-                resolve(blob);
-              }).catch((error) => {
-                let errorMessage = 'Request failed! Details: ' + ErrorMessageFormatter.format(error);
-                this.showServerError(errorMessage);
-                reject(error);
-              });
+              response
+                .blob()
+                .then((blob) => {
+                  resolve(blob);
+                })
+                .catch((error) => {
+                  let errorMessage =
+                    "Request failed! Details: " +
+                    ErrorMessageFormatter.format(error);
+                  this.showServerError(errorMessage);
+                  reject(error);
+                });
             }
           } else {
             if (response.status === 401) {
               localStorage.clear();
-              window.location.href = '/Login';
+              window.location.href = "/Login";
             }
             if (response.status === 304) {
               resolve();
             } else {
               if (response.status === 404) {
-                let errorMessage = 'Endpoint not found! Please contact dev team.';
+                let errorMessage =
+                  "Endpoint not found! Please contact dev team.";
                 this.showServerError(errorMessage);
                 reject(errorMessage);
               } else {
-                response.json().then((error) => {
-                  let errorMessage = 'Request failed! Details: ' + ErrorMessageFormatter.format(error);
-                  this.showServerError(errorMessage);
-                  reject(error);
-                }).catch((error) => {
-                  let errorMessage = 'Request failed! Details: ' + ErrorMessageFormatter.format(error);
-                  this.showServerError(errorMessage);
-                  reject(error);
-                });
+                response
+                  .json()
+                  .then((error) => {
+                    let errorMessage =
+                      "Request failed! Details: " +
+                      ErrorMessageFormatter.format(error);
+                    this.showServerError(errorMessage);
+                    reject(error);
+                  })
+                  .catch((error) => {
+                    let errorMessage =
+                      "Request failed! Details: " +
+                      ErrorMessageFormatter.format(error);
+                    this.showServerError(errorMessage);
+                    reject(error);
+                  });
               }
             }
           }
