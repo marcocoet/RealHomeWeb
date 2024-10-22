4b83c4c51c14c8762819ad1a6643b9d736853bd5
Timothy de Villiers <Timothy.deVilliers@avenutech.com>
2024-10-22 19:43:59 +0200

    Fix config

---
+++ src/config/config.js
@@ -0,0 +1,13 @@
+import configs from "./config.json";
+
+const hostname = window && window.location && window.location.hostname;
+let environment = "local";
+
+if (hostname == "realhome.com") environment = "cloud";
+
+const conf = {
+  realhome: configs[environment].realhome,
+  environment: environment,
+};
+
+export default conf;
